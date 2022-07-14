0c3f771442589dd438ea30a25ddfb7affaa150d0
Kazeem Quadri <105252349+Kezmandre@users.noreply.github.com>
2022-07-14 21:58:49 +0000

    check challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+  let sum = 0
+  for(let i = 0; i < arrayOfNumbers.length;i++) {
+    sum += arrayOfNumbers[i]
+  }
+  return sum
+  
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let even = 0
+  arrayOfNumbers.forEach(number=>{
+    if(number % 2 ===0){
+      even++;
+    }
+  });
+      return even;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
